98d27a1eabcb04f4aaf879f213a18ce7871d99a0
Hydronx101 <melvichiii@gmail.com>
2025-06-08 15:18:03 -0400

    Update staticObjLogic.cs
    Added ability to change layer during runtime

--- Assets/Scripts/staticObjLogic.cs
+++ Assets/Scripts/staticObjLogic.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class staticObjLogic : MonoBehaviour, IDamage
 {
     [SerializeField] Renderer model;
+    [SerializeField] int layer;
     [SerializeField] int HP;
     Color colorOrig;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -12,6 +13,7 @@ public class staticObjLogic : MonoBehaviour, IDamage
         colorOrig = model.material.color;
 
         changeTransparency();
+        changeLayer();
     }
 
     // Update is called once per frame
@@ -40,8 +42,14 @@ public class staticObjLogic : MonoBehaviour, IDamage
     }
     void changeTransparency()
     {
+        // Stores wall color and sets the transparency to half
+        // The material color must use transparent renduring
         Color temp = colorOrig;
         temp.a = 0.5f;
         model.material.color = temp;
+    }
+    void changeLayer()
+    {
+        gameObject.layer = layer;
     }
 }
