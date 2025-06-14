a4aacd95f56862ac6398f15989db636511ae1cc2
Brandon Eich <brandoneich93@gmail.com>
2025-06-13 18:44:30 -0700

    My attempt to have the sniper run from the player, as you can see he only stops functioning and doesnt run away when you get close

--- Assets/Scripts/enemyAI.cs
+++ Assets/Scripts/enemyAI.cs
@@ -17,6 +17,10 @@ public class enemyAI : MonoBehaviour, IDamage
     [SerializeField] GameObject bullet;
     [SerializeField] float shootRate;
 
+    [SerializeField] float fleeDistance;    // How close the player must be for the sniper to flee
+    [SerializeField] float safeDistance;    // How far sniper must be before resuming attack
+    [SerializeField] float fleeSpeed;       // Speed when fleeing
+
     Color colorOrig;
 
     float shootTimer;
@@ -24,13 +28,25 @@ public class enemyAI : MonoBehaviour, IDamage
 
     bool playerInRange;
 
+    bool isFleeing;         // Tracks if sniper is fleeing
+
     Vector3 playerDir;
+
+    Vector3 fleeDir;        // Sets direction of where the sniper flees
+    Vector3 fleeTarget;     // Sets the where the sniper flees to
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         colorOrig = model.material.color;
         gameManager.instance.updateGameGoal(1);
+
+        // Check if this enemy is a sniper
+        if (CompareTag("Sniper"))
+        {
+            // Start the fleeing behavior loop
+            StartCoroutine(fleeCheck());
+        }
 
     }
 
@@ -46,6 +62,8 @@ public class enemyAI : MonoBehaviour, IDamage
 
     bool canSeePlayer()
     {
+        if (isFleeing) return false; // Don't attack while fleeing
+
         playerDir = gameManager.instance.player.transform.position - headPos.position;
         angleToPlayer = Vector3.Angle(playerDir, transform.forward);
         Debug.DrawRay(headPos.position, playerDir);
@@ -125,5 +143,47 @@ public class enemyAI : MonoBehaviour, IDamage
 
 
         Instantiate(bullet, shootPos.position, transform.rotation);
+    }
+
+    // Checks if enemy should run away little bit at a time, not all the time
+    IEnumerator fleeCheck()
+    {
+        // Coroutine runs in the background continuously
+        while (true)
+        {
+            // Only run fleeing behavior if player is in range and this enemy is a Sniper
+            if (playerInRange && CompareTag("Sniper"))
+            {
+                // Measure how far the player is from the enemy
+                float distance = Vector3.Distance(transform.position, gameManager.instance.player.transform.position);
+                // If not already fleeing and the player is too close, start fleeing
+                if (!isFleeing && distance < fleeDistance)
+                {
+                    // Mark that the enemy is now fleeing
+                    isFleeing = true;
+                    // Change movement speed to fleeing speed
+                    agent.speed = fleeSpeed;
+                }
+                // If already fleeing and the player is far enough away, stop fleeing
+                else if (isFleeing && distance > safeDistance)
+                {
+                    // Mark that the enemy is no longer fleeing
+                    isFleeing = false;
+                }
+                // If currently fleeing, set a new direction and move away
+                if (isFleeing)
+                {
+                    // Calculate the direction directly away from the player
+                    fleeDir = (transform.position - gameManager.instance.player.transform.position).normalized;
+                    // Set the target point to move away from the player
+                    fleeTarget = transform.position + fleeDir * 10f;
+                    // Tell the NavMeshAgent to go to the flee target
+                    agent.SetDestination(fleeTarget);
+                }
+            }
+
+            // Wait a short time before checking again to avoid overloading the game with updates
+            yield return new WaitForSeconds(0.2f);
+        }
     }
 }
