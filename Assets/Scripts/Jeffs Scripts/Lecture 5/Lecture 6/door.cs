97cce6ebc22c03f63cac4ab54b04e496c4e89d75
chromeclouds <55388703+chromeclouds@users.noreply.github.com>
2025-06-24 01:12:26 -0400

    WIP before pulling safety. made sure everything is prefabbed right, fixed issue with doors, fixed issue with weaponholder having player tag, ammo now buys infinitely if points allow, file clean up and organization, moved stuff in dev area

--- Assets/Scripts/Jeffs Scripts/Lecture 5/Lecture 6/door.cs
+++ Assets/Scripts/Jeffs Scripts/Lecture 5/Lecture 6/door.cs
@@ -14,11 +14,24 @@ public class door : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        IOpen open = other.GetComponent<IOpen>();
 
+        if (open != null)
+        {
+            doorModel.SetActive(false);
+        }
+        //commented out zacks edit just to get door to open for time being.
+        //this needs to be set up so that doors are locked during rounds
+        //unlocked after all enemies are dead and waiting for new round to start
+        //should start new round after player enters castle and exits
+        //doors lock behind, start round.
+
+        /*
         if (gameManager.instance.gameGoalCount <= 0)
         {
             doorModel.SetActive(false);
         }
+        */
     }
 
     private void OnTriggerExit(Collider other)
