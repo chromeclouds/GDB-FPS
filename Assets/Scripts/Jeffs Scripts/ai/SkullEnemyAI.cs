e32ba4b801decc92897818fce2ae49b3c94e12bb
Brandon Eich <brandoneich93@gmail.com>
2025-07-13 17:16:20 -0700

    Forgot to make the Skull demon destroyable when shot, fixed

--- Assets/Scripts/Jeffs Scripts/ai/SkullEnemyAI.cs
+++ Assets/Scripts/Jeffs Scripts/ai/SkullEnemyAI.cs
@@ -1,22 +1,35 @@
 using UnityEngine;
 using UnityEngine.AI;
 using System.Collections.Generic;
+using System.Collections;
 
-public class SkullEnemyAI : MonoBehaviour
+public class SkullEnemyAI : MonoBehaviour, IDamage
 {
     [SerializeField] private Transform gateTarget;
     [SerializeField] private GameObject explosionEffect;
     [SerializeField] private float explodeDistance = 1.0f;
     [SerializeField] private int damageToGate = 50;
 
+    [SerializeField] private int maxHealth;
+    [SerializeField] private Renderer model;
+
     private NavMeshAgent agent;
     private bool hasExploded = false;
+    private int currentHealth;
+
+    private Color colorOrig;
 
     public List<LectureEnemyAI> lowlyDemons = new List<LectureEnemyAI>();
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        currentHealth = maxHealth;
+
+        if (model != null)
+        {
+            colorOrig = model.material.color;
+        }
 
         if (gateTarget != null)
         {
@@ -71,5 +84,28 @@ public class SkullEnemyAI : MonoBehaviour
     public void AssignGateTarget(Transform target)
     {
         gateTarget = target;
+    }
+
+    public void takeDamage(int amount)
+    {
+        if (hasExploded) return;
+
+        currentHealth -= amount;
+        agent.SetDestination(gameManager.instance.player.transform.position);
+
+        if (currentHealth <= 0)
+        {
+            Explode();
+        }
+        else
+        {
+            StartCoroutine(flashRed());
+        }
+    }
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrig;
     }
 }
