bc22e64b1b87fdf7400da91d059b0c07c359c38d
MrIsaiah <MDBennett1@student.fullsail.edu>
2025-07-18 11:43:36 -0400

    CreditsScroll (Done)

--- Assets/Scripts/Marks Scripts/CreditsScroll.cs
+++ Assets/Scripts/Marks Scripts/CreditsScroll.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class CreditsScroll
+public class CreditsScroll : MonoBehaviour
 {
-    
+    public float scrollSpeed = 30f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.up * scrollSpeed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MainMenu"); 
+        }
+    }
 }
